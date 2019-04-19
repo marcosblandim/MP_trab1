034f6cffacd29cef4dc73e722a55bda612f0640c
MarcosBlandim <marcosblandim@gmail.com>
2019-04-19 20:04:57 -0300

    Main para aplicação da função.

--- romano.cpp
+++ romano.cpp
@@ -1,14 +1,20 @@
-// Copyright 2019 Marcos
+// Copyright 2019 Marcos.
 #include <iostream>
 #include <string>
 #include "romano.hpp"
 using namespace std;
 
 int main() {
+
   int numArab;
   string numRoman;
+  cout << "Digite um número em algarismos romanos:" << endl;
   cin >> numRoman;
-  numArab = convertRomano(numRoman);
-  cout << numArab << endl;
+  numArab = ConverteRomano(numRoman);
+  if (numArab == -1) {
+    cout << "Digitação inválida." << endl;
+  } else {
+    cout << "Seu equivalente em algarismos arábicos é: " << numArab << endl;
+  }
   return 0;
 }
